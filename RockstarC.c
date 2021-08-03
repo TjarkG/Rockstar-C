0aa2faeaa6907ec32430e997a1baa58e5f362db8
TjarkG <tjark.gaudich@outlook.de>
2021-08-03 21:13:45 +0200

    changed aproche, added Parsing to new File

--- RockstarC.c
+++ RockstarC.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdbool.h>
 
 #define MAXL 32
 
@@ -25,14 +26,42 @@ const char *foos[] = {
     "short",
     "char",
 };
+const char separators[] = {
+    '(',
+    ')',
+    '{',
+    '}',
+    '[',
+    ']',
+    ';'
+};
+const char calcs[] = {
+    '+',
+    '-',
+    '*',
+    '/',
+    '%',
+    '=',
+    ',',
+    '<',
+    '>',
+    '|',
+    '&',
+    '~',
+    '?',
+    '!'
+};
 
-void filecopy(FILE *, FILE *);
+void parseC(FILE *, FILE *);
+void removeEnding(const char*, char*);
 void toCamelCase(char *);
 void convert_to_words(char*, char*);
+bool iscalc(char);
 
 int main(int argc, char *argv[])
 {
-    FILE *fp;
+    FILE *fpIn;
+    FILE *fpParse;
     char *prog = argv[0];     // program name for errors
 
     if (argc == 1 ) /* no args: throw error */
@@ -42,26 +71,23 @@ int main(int argc, char *argv[])
     }
     while (--argc > 0)
     {
-        if ((fp = fopen(*++argv, "r")) == NULL) 
+        ++argv;
+        if ((fpIn = fopen(*argv, "r")) == NULL) 
         {
             fprintf(stderr, "%s: can't open %s\n", prog, *argv);
             exit(1);
         }
         else
         {
-            filecopy(fp, stdout);
-            fclose(fp);
+            char parseName[strlen(*argv)+4];
+            removeEnding(*argv, parseName);
+            strcpy(parseName+strlen(parseName)-1, ".parse");
+            fpParse = fopen(parseName, "w");
+            parseC(fpIn, fpParse);
+            fclose(fpParse);
+            fclose(fpIn);
         }
     }
-    char c[] = "test_name12";
-    toCamelCase(c);
-    printf("%s\n", c);
-    char d[] = "testtest30_name";
-    toCamelCase(d);
-    printf("%s\n", d);
-    char e[] = "20";
-    toCamelCase(e);
-    printf("%s\n", e);
     if (ferror(stdout))
     {
         fprintf(stderr, "%s: error writing stdout\n", prog);
@@ -70,133 +96,92 @@ int main(int argc, char *argv[])
     exit(0);
 }
 
-void filecopy(FILE *ifp, FILE *ofp)
+void parseC(FILE *ifp, FILE *ofp)
 {
-    char c[MAXL];
-    char indent = 0;
-    while ( fscanf(ifp, "%s[a-z]", c) != EOF)
+    char in;
+    char inLast = '\0';
+    bool isSep;
+    bool wasSep = false;
+    char inComment = 0;     // 0:no comment, 1: one line comment, 2: multi Line comment, 3: Include, 4: String Literal 5: Char Literal
+    while ((in = getc(ifp)) != EOF)
     {
-        for (char i = 0; i < (sizeof(ignore) / sizeof(char *)); i++)        //typs get ignored
-            if(strcmp(c, ignore[i]) == 0)
-            {
-                c[0] = '\0';
-                break;
-            }
+        isSep = false;
+        char cmtOld = inComment;
+        if(inLast == '/' && in == '/')
+            inComment = 1;
+        else if(inLast == '/' && in == '*')
+            inComment = 2;
+        else if(in == '<')
+            inComment = 3;
+        else if(in == '"' && inComment != 4)
+            inComment = 4;
+        else if(in == '\'' && inComment != 4)
+            inComment = 5;
+        else if(inComment == 1 && in == '\n')
+            inComment = 0;
+        else if(inComment == 2 && in == '/' && inLast == '*')
+            inComment = 0;
+        else if(inComment == 3 && in == '>')
+            inComment = 0;
+        else if(inComment == 4 && in == '"' && inLast != '\\')
+            inComment = 0;
+        else if(inComment == 5 && in == '\'' && inLast != '\\')
+            inComment = 0;
         
-        for (char i = 0; i < (sizeof(foos) / sizeof(char *)); i++)
+        if(inComment < cmtOld)
+            fprintf(ofp, "\n");
+
+        if(!inComment)
         {
-            if(strcmp(c, foos[i]) == 0)
+            for (char i = 0; i < sizeof(separators); i++)
             {
-                if(!indent)
+                if(in == separators[i])
                 {
-                    c[0] = '\0';
-                    indent ++;
+                    isSep = true;
+                    break;
                 }
-                else
-                {
-                    fscanf(ifp, "%s ", c);
-                    for (char j = 0; j < (sizeof(foos) / sizeof(char *)); j++)
-                    {
-                        if(strcmp(c, foos[j]) == 0)
-                        {
-                            fscanf(ifp, "%s ", c);
-                            break;
-                        }
-                    }
-                    
-                    char temp;
-                    if(temp = getc(ifp) == '=')
-                    {
-                        fprintf(ofp,"%s is ", c);
-                        fscanf(ifp, "%s[a-z]", c);
-                    }
-                    else
-                    {
-                        fscanf(ifp, "%s ", c+1);
-                        c[0] = temp;
-                    }
-                }
-                //fprintf(ofp," \n%d\n", i);
-                //fprintf(ofp," %s\n", c);
-                break;
             }
         }
+        if((isSep && wasSep) || (isSep && (inLast == '\n')))
+            fprintf(ofp, "%c\n", in);
+        else if(isSep)
+            fprintf(ofp, "\n%c\n", in);
+        else if(in == '\n' && wasSep)
+            ;
+        else if(isspace(in) && isspace(inLast) && !inComment)
+            ;
+        else if(isspace(in) && ( isalpha(inLast) || iscalc(inLast)) && !inComment)
+            fprintf(ofp, "\n");
+        else
+            fprintf(ofp, "%c", in);
 
-        if(strcmp(c, "return") == 0)
-        {
-            fprintf(ofp,"Give Back ");
-            fscanf(ifp, " %s ;", c);
-            if(c[strlen(c)-1] == ';')
-            {
-                c[strlen(c)-1] = '\0';
-            }
-        }
-        else if(strcmp(c, "#include") == 0)
-        {
-            fscanf(ifp, " <%[a-z]s", c);
-            fscanf(ifp, ".h>");
-            c[0] ='\0';
-        }
-        else if(strcmp(c, "}") == 0)
-        {
-            indent--;
-            c[0] ='\0';
-        }
-        else if(strncmp(c, "printf", (sizeof("printf")-1)) == 0)
-        {
-            if(c[strlen(c)-1] != ';')
-            {
-                char in;
-                int lenght = strlen(c);
-                c[lenght++] = ' ';
-                while ((in = getc(ifp)) != ')')
-                    c[lenght++] = in;
-                c[lenght] = '\0';
-            }
-            else
-            {
-                c[strlen(c)-2] = '\0';
-            }
-            fprintf(ofp,"Say %s\n", c+sizeof("printf"));
-            c[0] = '\0';
-        }
-        else if(strncmp(c, "//", (sizeof("//")-1)) == 0)
-        {
-            strcpy(c, c+1);
-            c[0] = '(';
-            char in;
-            int lenght = strlen(c);
-            c[lenght++] = ' ';
-            while ((in = getc(ifp)) != '\n')
-                c[lenght++] = in;
-            c[lenght++] = ')';
-            c[lenght] = '\0';
-        }
-        else if(strncmp(c, "/*", (sizeof("/*")-1)) == 0)
-        {
-            strcpy(c, c+1);
-            c[0] = '(';
-            char in;
-            int lenght = strlen(c);
-            c[lenght++] = ' ';
-            do
-            {
-                in = getc(ifp);
-                c[lenght] = in;
-                lenght++;
-            } while ( strncmp("*/",c+lenght-2, 2) && lenght < MAXL);
-
-            c[lenght-2] = ')';
-            c[lenght-1] = '\0';
-        }
-
-        if(c[strlen(c)-1] == ';')
-        {
-            c[strlen(c)-1] = '\0';
-        }
-        if(!c[0] == '\0')
-            fprintf(ofp,"%s\n", c);
+        if(inComment > cmtOld)
+            fprintf(ofp, "\n");
+        inLast = in;
+        wasSep = isSep;
     }
+}
+
+bool iscalc(char c)
+{
+    for (char i = 0; i < sizeof(calcs); i++)
+        {
+            if(c == calcs[i])
+                return true;
+        }
+    return false;
+}
+
+void removeEnding(const char* in, char* out)
+{
+    char length = (strlen(in)-1);
+    strcpy(out, in);
+    for (char i = length; i >= 0; i--)
+        if(in[i] == '.')
+        {
+            out[i] = '\0';
+            return;
+        }
 }
 
 void toCamelCase(char *name)
--- TestTarget.c
+++ TestTarget.c
@@ -2,13 +2,15 @@
 
 int main(void)
 {
-    //This is a Comment
-    /*and this too.*/
+    //This is a Comment()
+    /*and this too.
+    int main(void)
+    {*/
     int i = 6;
     int j, k;
     double test = 3.54;
     volatile unsigned long int testTwo = 12432563283;
-    //test = test + i;
+    test = test + i;
     printf("Hello World\n");
     return 0;
 }
