38437914e8de3cc8280383a44d25328ddc7a2dc8
TjarkG <tjark.gaudich@outlook.de>
2021-08-04 19:50:13 +0200

    put Parser in seperate File, started Converting

---
+++ Parse.h
@@ -0,0 +1,118 @@
+/*
+ * Parse.h
+ *
+ * Header to parse c to the format needed for the c to rockstar converter
+ *
+ * Created: 04.08.2021 19:40:05
+ *  Author: TjarkG
+ */
+
+#ifndef Parse_H_
+#define Parse_H_
+
+const char separators[] = {
+    '(',
+    ')',
+    '{',
+    '}',
+    '[',
+    ']',
+    ';'
+};
+const char calcs[] = {
+    '+',
+    '-',
+    '*',
+    '/',
+    '%',
+    '=',
+    ',',
+    '<',
+    '>',
+    '|',
+    '&',
+    '~',
+    '?',
+    '!'
+};
+
+void parseC(FILE *, FILE *);
+bool iscalc(char);
+
+void parseC(FILE *ifp, FILE *ofp)
+{
+    char in;
+    char inLast = '\0';
+    bool isSep;
+    bool wasSep = false;
+    char inComment = 0;     // 0:no comment, 1: one line comment, 2: multi Line comment, 3: Include, 4: String Literal 5: Char Literal
+    while ((in = getc(ifp)) != EOF)
+    {
+        isSep = false;
+        char cmtOld = inComment;
+        if(inLast == '/' && in == '/')
+            inComment = 1;
+        else if(inLast == '/' && in == '*')
+            inComment = 2;
+        else if(in == '<')
+            inComment = 3;
+        else if(in == '"' && inComment != 4)
+            inComment = 4;
+        else if(in == '\'' && inComment != 4)
+            inComment = 5;
+        else if(inComment == 1 && in == '\n')
+            inComment = 0;
+        else if(inComment == 2 && in == '/' && inLast == '*')
+            inComment = 0;
+        else if(inComment == 3 && in == '>')
+            inComment = 0;
+        else if(inComment == 4 && in == '"' && inLast != '\\')
+            inComment = 0;
+        else if(inComment == 5 && in == '\'' && inLast != '\\')
+            inComment = 0;
+        
+        if(inComment < cmtOld)
+            fprintf(ofp, "\n");
+
+        if(!inComment)
+        {
+            for (char i = 0; i < sizeof(separators); i++)
+            {
+                if(in == separators[i])
+                {
+                    isSep = true;
+                    break;
+                }
+            }
+        }
+        if((isSep && wasSep) || (isSep && (inLast == '\n')))
+            fprintf(ofp, "%c\n", in);
+        else if(isSep)
+            fprintf(ofp, "\n%c\n", in);
+        else if(in == '\n' && wasSep)
+            ;
+        else if(isspace(in) && isspace(inLast) && !inComment)
+            ;
+        else if(isspace(in) && ( isalpha(inLast) || iscalc(inLast)) && !inComment)
+            fprintf(ofp, "\n");
+        else
+            fprintf(ofp, "%c", in);
+
+        if(inComment > cmtOld)
+            fprintf(ofp, "\n");
+        inLast = in;
+        wasSep = isSep;
+    }
+}
+
+bool iscalc(char c)
+{
+    for (char i = 0; i < sizeof(calcs); i++)
+        {
+            if(c == calcs[i])
+                return true;
+        }
+    return false;
+}
+
+#endif /* Parse_H_ */
--- RockstarC.c
+++ RockstarC.c
@@ -1,12 +1,22 @@
+/*
+ * RockstarC.h
+ *
+ * programm to convert c to rockstar code
+ *
+ * Created: 02.08.2021 15:42:57
+ *  Author: TjarkG
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 #include <stdbool.h>
+#include "Parse.h"
 
 #define MAXL 32
 
-const char *ignore[] = {
+/*const char *ignore[] = {
     "auto",
     "register",
     "const",
@@ -16,7 +26,7 @@ const char *ignore[] = {
     "volatile",
     "main(void)",
     "{"
-};
+};*/
 
 const char *foos[] = {
     "double",
@@ -26,38 +36,11 @@ const char *foos[] = {
     "short",
     "char",
 };
-const char separators[] = {
-    '(',
-    ')',
-    '{',
-    '}',
-    '[',
-    ']',
-    ';'
-};
-const char calcs[] = {
-    '+',
-    '-',
-    '*',
-    '/',
-    '%',
-    '=',
-    ',',
-    '<',
-    '>',
-    '|',
-    '&',
-    '~',
-    '?',
-    '!'
-};
 
-void parseC(FILE *, FILE *);
 void rockstarConvert(FILE *, FILE *);
 void removeEnding(const char*, char*);
 void toCamelCase(char *);
 void convert_to_words(char*, char*);
-bool iscalc(char);
 int getWord(FILE *, char*, char);
 
 int main(int argc, char *argv[])
@@ -100,11 +83,6 @@ int main(int argc, char *argv[])
             fclose(fpOut);
         }
     }
-    if (ferror(stdout))
-    {
-        fprintf(stderr, "%s: error writing stdout\n", prog);
-        exit(2);
-    }
     exit(0);
 }
 
@@ -113,84 +91,19 @@ void rockstarConvert(FILE *ifp, FILE *ofp)
     char in[MAXL];
     while (getWord(ifp, in, MAXL) != EOF)
     {
+        if(strcmp(in, "#include") == 0)
+        {
+            getWord(ifp, in, MAXL);
+            if(strcmp(in, "<"))
+                fprintf(stderr, "conversion Error: #include not followed by <");
+            getWord(ifp, in, MAXL);
+            getWord(ifp, in, MAXL);
+            if(strcmp(in, ">"))
+                fprintf(stderr, "conversion Error: #include not followed by >");
+            continue;
+        }
         fprintf(ofp,"%s\n",in);
     }  
-}
-
-void parseC(FILE *ifp, FILE *ofp)
-{
-    char in;
-    char inLast = '\0';
-    bool isSep;
-    bool wasSep = false;
-    char inComment = 0;     // 0:no comment, 1: one line comment, 2: multi Line comment, 3: Include, 4: String Literal 5: Char Literal
-    while ((in = getc(ifp)) != EOF)
-    {
-        isSep = false;
-        char cmtOld = inComment;
-        if(inLast == '/' && in == '/')
-            inComment = 1;
-        else if(inLast == '/' && in == '*')
-            inComment = 2;
-        else if(in == '<')
-            inComment = 3;
-        else if(in == '"' && inComment != 4)
-            inComment = 4;
-        else if(in == '\'' && inComment != 4)
-            inComment = 5;
-        else if(inComment == 1 && in == '\n')
-            inComment = 0;
-        else if(inComment == 2 && in == '/' && inLast == '*')
-            inComment = 0;
-        else if(inComment == 3 && in == '>')
-            inComment = 0;
-        else if(inComment == 4 && in == '"' && inLast != '\\')
-            inComment = 0;
-        else if(inComment == 5 && in == '\'' && inLast != '\\')
-            inComment = 0;
-        
-        if(inComment < cmtOld)
-            fprintf(ofp, "\n");
-
-        if(!inComment)
-        {
-            for (char i = 0; i < sizeof(separators); i++)
-            {
-                if(in == separators[i])
-                {
-                    isSep = true;
-                    break;
-                }
-            }
-        }
-        if((isSep && wasSep) || (isSep && (inLast == '\n')))
-            fprintf(ofp, "%c\n", in);
-        else if(isSep)
-            fprintf(ofp, "\n%c\n", in);
-        else if(in == '\n' && wasSep)
-            ;
-        else if(isspace(in) && isspace(inLast) && !inComment)
-            ;
-        else if(isspace(in) && ( isalpha(inLast) || iscalc(inLast)) && !inComment)
-            fprintf(ofp, "\n");
-        else
-            fprintf(ofp, "%c", in);
-
-        if(inComment > cmtOld)
-            fprintf(ofp, "\n");
-        inLast = in;
-        wasSep = isSep;
-    }
-}
-
-bool iscalc(char c)
-{
-    for (char i = 0; i < sizeof(calcs); i++)
-        {
-            if(c == calcs[i])
-                return true;
-        }
-    return false;
 }
 
 void removeEnding(const char* in, char* out)
